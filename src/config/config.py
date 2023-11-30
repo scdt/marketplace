4f9bb637f277b8b42654707e33a6e8d92232cacf
hmbefi <hmbefi@email.com>
2023-11-30 13:29:49 +0700

    добавлена конфигуряция проекта

---
+++ src/config/config.py
@@ -0,0 +1,43 @@
+"""
+Модуль для конфигурации проекта.
+
+Позволяет импортировать конфигурацию из config.yml файла.
+"""
+
+from pathlib import Path
+
+import yaml
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class _SettingsModel(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix='EMP_', env_nested_delimiter='_')
+
+    @classmethod
+    def from_yaml(cls, config_path: str) -> '_SettingsModel':
+        return cls(**yaml.safe_load(Path(config_path).read_text()))
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls,
+        init_settings,
+        env_settings,
+        dotenv_settings,
+        file_secret_settings,
+    ):
+        return env_settings, init_settings, file_secret_settings
+
+
+class _ServiceSettings(_SettingsModel):
+    host: str
+    port: int
+
+
+class Settings(_SettingsModel):
+    """Настройки сервиса."""
+
+    service: _ServiceSettings
+
+
+settings = Settings.from_yaml('src/config/config.yml')
