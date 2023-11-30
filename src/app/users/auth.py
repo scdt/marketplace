a1ee8dba2c6df505e675e37c2b9b9332dc101190
hmbefi <hmbefi@email.com>
2023-11-30 16:48:49 +0700

    добавлен функционал проверки пароля

---
+++ src/app/users/auth.py
@@ -0,0 +1,22 @@
+"""Модуль содержащий функции для аутентификации пользователя."""
+
+from bcrypt import checkpw
+
+from src.app.data_sources.storages.user_storage import UserStorage
+
+
+async def authenticate_user(storage: UserStorage, username: str, password: str) -> bool:
+    """Метод для проверки пароля пользователя.
+
+    Args:
+        storage (UserStorage): хранилище пользователей
+        username (str): имя пользователя
+        password (str): пароль пользователя
+
+    Returns:
+        bool: результат проверки
+    """
+    user = storage.get_user(username=username)
+    if not user:
+        return False
+    return checkpw(password.encode(), user.password_hash.encode())
